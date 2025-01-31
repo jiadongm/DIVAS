04fcf2694156d68d0e55fafab0a1077e14e657ab
Jiadong Mao <chiatungmao@gmail.com>
2025-01-31 11:30:02 +1100

    Update JointSturctPenaltyCCPJEarlyStop.R

--- R/JointSturctPenaltyCCPJEarlyStop.R
+++ R/JointSturctPenaltyCCPJEarlyStop.R
@@ -19,23 +19,23 @@ ccpSubOptJPEarlyStop <- function(v0, Qo1, Qo2, Qc1, Qc2, Vo, tau) {
   v <- CVXR::Variable(n)
   slack <- CVXR::Variable(nc + 2)
 
-  objective <- CVXR::quad_form(v, Qo1) - 2 * t(v0) %*% Qo2 %*% v + CVXR::quad_form(v0, Qo2) + tau * sum(slack)
+  objective <- CVXR::quad_form(v, Qo1) - 2 * CVXR::t(v0) %*% Qo2 %*% v + CVXR::quad_form(v0, Qo2) + tau * sum(slack)
   constraints <- list()
 
   # constraints for each Qc1 and Qc2
   for (ic in 1:nc) {
     constraints <- c(constraints,
-                     CVXR::quad_form(v, Qc1[[ic]]) - 2 * t(v0) %*% Qc2[[ic]] %*% v + CVXR::quad_form(v0, Qc2[[ic]]) <= slack[ic])
+                     CVXR::quad_form(v, Qc1[[ic]]) - 2 * CVXR::t(v0) %*% Qc2[[ic]] %*% v + CVXR::quad_form(v0, Qc2[[ic]]) <= slack[ic])
   }
   # Addition
   constraints <- c(constraints,
                    CVXR::sum_squares(v) - 1 <= slack[nc + 1],
-                   1 - 2 * t(v0) %*% v + CVXR::sum_squares(v) <= slack[nc + 2],
+                   1 - 2 * CVXR::t(v0) %*% v + CVXR::sum_squares(v) <= slack[nc + 2],
                    slack >= 0,
-                   t(Vo) %*% v == matrix(0, nrow = ro, ncol = 1))
+                   CVXR::t(Vo) %*% v == matrix(0, nrow = ro, ncol = 1))
   # cvx_end
 
-  problem <- CVXR::Problem(Minimize(objective), constraints)
+  problem <- CVXR::Problem(CVXR::Minimize(objective), constraints)
   result <- CVXR::solve(problem)
   #result <- solve(problem, solver = "ECOS")
   v_opt <- result$getValue(v)
